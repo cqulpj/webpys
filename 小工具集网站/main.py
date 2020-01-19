ef9a80fab2f5ec820fe07d68648b5dc94723f255
lpj <lpj19851224@163.com>
2020-01-19 23:24:33 +0800

    调整了小工具集网站的文件结构并修复了一处bug

--- 小工具集网站/main.py
+++ 小工具集网站/main.py
@@ -7,6 +7,7 @@ import json
 import base64
 from web.contrib.template import render_jinja
 import dealops
+from apis import *
 
 # 路径映射
 urls = (
@@ -42,83 +43,6 @@ class index:
     def GET(self):
         return render.index()
 
-# 下面是一系列数据转换或编解码API
-# hex与ascii字符串互转
-class hexascii:
-    def POST(self):
-        pld = web.input()
-        print(pld)
-        ret = {}
-        try:
-            # hex转ascii
-            if pld.ops == '0':
-                ss = pld.data.strip().replace(' ', '')
-                cs = []
-                for i in range(0, len(ss), 2):
-                    tt = int(ss[i:i+2], 16)
-                    cs.append(chr(tt))
-                ret['data'] = ''.join(cs).decode('gbk')
-                ret['status'] = 'ok'
-            # ascii字符串转hex
-            elif pld.ops == '1':
-                ss = pld.data.strip().encode('gbk')
-                d = [hex(ord(i)).upper()[2:4] for i in ss]
-                ss = ' '.join(d)
-                ret['data'] = ss
-                ret['status'] = 'ok'
-            # 无效操作
-            else:
-                ret['status'] = 'invalid ops'
-                ret['data'] = ''
-        except Exception as e:
-            print(e)
-            ret['status'] = 'error'
-            ret['data'] = ''
-
-        return json.dumps(ret)
-
-# hex数据的base64编解码
-class hexbase64:
-    def POST(self):
-        pld = web.input()
-        print(pld)
-        ret = {}
-        try:
-            # 字符串直接base64编码
-            if pld.ops == '0':
-                ret['data'] = base64.b64encode(pld.data)
-                ret['status'] = 'ok'
-            # hex形式数组base64编码
-            elif pld.ops == '1':
-                ss = pld.data.strip().replace(' ', '')
-                cs = []
-                for i in range(0, len(ss), 2):
-                    tt = int(ss[i:i+2], 16)
-                    cs.append(chr(tt))
-                ret['data'] = base64.b64encode(''.join(cs))
-                ret['status'] = 'ok'
-            # base64解码为字符串 
-            elif pld.ops == '2':
-                ss = pld.data.strip()
-                ret['data'] = base64.b64decode(ss)
-                ret['status'] = 'ok'
-            # base64解码为hex形式数组
-            elif pld.ops == '3':
-                ss = pld.data.strip()
-                ss = base64.b64decode(ss)
-                bs = [hex(ord(i)).upper()[2:] for i in ss]
-                ret['data'] = ' '.join(bs)
-                ret['status'] = 'ok'
-            # 无效操作
-            else:
-                ret['status'] = 'invalid ops'
-                ret['data'] = ''
-        except Exception as e:
-            print(e)
-            ret['status'] = 'error'
-            ret['data'] = ''
-
-        return json.dumps(ret)
 
 if __name__ == '__main__':
     app.run()
